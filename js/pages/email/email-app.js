95518d7664c4571b9deb6a05d5e834c5e06cbf71
Ilana Mostovski <ilana.mostovski@gmail.com>
2018-04-11 19:36:09 +0300

    email-details

--- js/pages/email/email-app.js
+++ js/pages/email/email-app.js
@@ -1,25 +1,59 @@
 import emailService from '../../services/email.service.js'
 import emailList from '../../cmps/email/email-list.js'
+import emailDetails from '../../cmps/email/email-details.js'
 
 
 export default {
     data(){
         return {
-            emails: []
+            emails: [],
+            selectedEmail: null
         }
     },
 
     template: `<section>
         <h1>email-app</h1>
-        <email-list :emails="emails"> </email-list>
+        <email-list :emails="emails" @selected="selectEmail"> </email-list>
+        <email-details :email="selectedEmail"> </email-details>
     </section>`,
 
     created() {
+        debugger;
         emailService.getEmails()
             .then(emails => this.emails = emails);
+
+        this.selectedEmail = this.emails[0];
+    },
+
+    computed:{
+        // emailToShow() {
+        //     debugger;
+        //     if(!this.selectedEmail) {
+        //         return this.emails[0];
+        //     } else{
+        //         return this.selectedEmail;
+        //     }
+        // }
+    },
+
+    methods:{
+        selectEmail(emailId) {
+            var email = this.emails.find(email => email.id === emailId);
+            this.selectedEmail = email;
+        }
+
+        // emailToShow() {
+        //     debugger;
+        //     if(!this.selectedEmail) {
+        //         return this.emails[0];
+        //     } else{
+        //         return this.selectedEmail;
+        //     }
+        // }
     },
 
     components: {
-        emailList
+        emailList,
+        emailDetails
     }
 };
