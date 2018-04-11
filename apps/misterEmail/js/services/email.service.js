5ef5430f1ffd4d4cb9b265e307cc88df071b36e8
Ilana Mostovski <ilana.mostovski@gmail.com>
2018-04-11 15:50:49 +0300

    componnets and pages added to apps

---
+++ apps/misterEmail/js/services/email.service.js
@@ -0,0 +1,37 @@
+import utilService from './util.service.js'
+import storageService from './storage.service.js'
+import eventBus, {USR_MSG_DISPLAY} from './event-bus.service.js'
+
+var emailsDB = [];
+
+const EMAILS_KEY = 'emailsApp';
+
+function getEmails() {
+    return new Promise((resolve, reject) => {
+        var emails = storageService.load(EMAILS_KEY);
+        if (!emails) {
+            emails = generatebooks();
+            storageService.store(EMAILS_KEY, emails)
+        }
+        emailsDB = emails;
+        // emitEmailsCount();
+        resolve(emailsDB);
+    });
+}
+
+
+
+
+function generateEmails() {
+    var emails = []
+    for (let index = 0; index < 20; index++) {
+        var email = createEmail()
+        emails.push(email)
+
+    }
+    return emails;
+}
+
+
+
+
