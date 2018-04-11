42785bbf53062bb737272d49bf04c98d85651a71
Ilana Mostovski <ilana.mostovski@gmail.com>
2018-04-11 15:33:07 +0300

    main structure created

--- js/routes.js
+++ js/routes.js
@@ -1,10 +1,16 @@
 import home from './pages/home.js'
 import about from './pages/about.js'
+import misterEmail from '../apps/misterEmail/js/pages/home.js'
+import misterPlace from '../apps/misterPlace/js/pages/home.js'
+import misterKeeper from '../apps/misterKeeper/js/pages/home.js'
 
 
 const routes = [
     {path: '/', component: home},
     {path: '/about', component: about},
+    {path: '/misterEmail', component: misterEmail},
+    {path: '/misterPlace', component: misterPlace},
+    {path: '/misterKeeper', component: misterKeeper}
   ];
 
 Vue.use(VueRouter);
