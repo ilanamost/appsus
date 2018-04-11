5aa4dd512c21458864ef373f0177d101a37fcc71
Nora Marcelli <noramarcelli@gmail.com>
2018-04-11 17:01:50 +0300

    change folder structure

--- apps/misterKeeper/js/services/event-bus.service.js
+++
@@ -1,5 +0,0 @@
-export const USR_MSG_DISPLAY = 'usr-msg-display';
-
-var bus = new Vue();
-
-export default bus;
--- apps/misterPlace/js/pages/home.js
+++
@@ -1,7 +0,0 @@
-export default {
-    template:`
-    <section class="home">
-        <h1>Home!</h1>
-    </section>
-    `
-}
