5df6a1e43b885884f432a08461e286b846b42c15
Nora Marcelli <noramarcelli@gmail.com>
2018-04-12 16:53:38 +0300

    createPlace func + pics

--- js/services/place.service.js
+++ js/services/place.service.js
@@ -1,8 +1,10 @@
 import utilService from './util.service.js'
 import storageService from './storage.service.js'
-import eventBus, {USR_MSG_DISPLAY} from './event-bus.service.js'
+import eventBus, { USR_MSG_DISPLAY } from './event-bus.service.js'
 
 var placessDB = [];
+var tagsDB = ['Fun', 'Food', 'Work', 'Romantic', 'Music', 'Dance', 'Extrim', 'Family', 'Children'];
+
 const PLACES_KEY = 'placeApp';
 
 
@@ -19,25 +21,33 @@ function getPlaces() {
 }
 
 
-function generateEmails() {
-    var emails = []
-    for (let index = 0; index < 20; index++) {
-        var email = createEmail(index + 1)
-        emails.push(email)
+function generatePlaces() {
+    var places = []
+    for (let index = 0; index < 7; index++) {
+        var place = createPlace(index + 1)
+        places.push(place)
     }
-    return emails;
+    return places;
 }
 
 
-function createPlace(coords) {
+function createPlace(lat, lng) {
     var loremIpsum = new LoremIpsum();
 
-    var email = {
+    var place = {
         id: i,
-        subject: loremIpsum.generate(utilService.getRandomInt(1, 4), utilService.getRandomInt(3, 6)),
-        sentAt: utilService.randomDate(new Date(2012, 0, 1), new Date()).valueOf(),
-        body: loremIpsum.generate(utilService.getRandomInt(10, 30), utilService.getRandomInt(1, 4)),
-        isRead: false,
+        name: loremIpsum.generate(utilService.getRandomInt(1, 4), utilService.getRandomInt(3, 6)),
+        // sentAt: utilService.randomDate(new Date(2012, 0, 1), new Date()).valueOf(),
+        desc: loremIpsum.generate(utilService.getRandomInt(10, 30), utilService.getRandomInt(1, 4)),
+        tags: [],
+        imgUrl: `img/${utilService.getRandomInt(1, 20)}.jpg`,
+
+        // isRead: false,
     }
-    return email;
+    var randomTagIdx = utilService.getRandomInt(0, tagsDB.length);
+    for (var i = 0; i < randomTagIdx; i++) {
+        var tag = tagsDB[randomTagIdx];
+        tags.push(tag);
+    }
+    return place;
 }
