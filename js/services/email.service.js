b39be1255be22f6334f207ef5383c1aa13b0c746
Nora Marcelli <noramarcelli@gmail.com>
2018-04-12 14:08:08 +0300

    read/unread

--- js/services/email.service.js
+++ js/services/email.service.js
@@ -1,6 +1,6 @@
 import utilService from './util.service.js'
 import storageService from './storage.service.js'
-import eventBus, {USR_MSG_DISPLAY, EMAILS_COUNT} from './event-bus.service.js'
+import eventBus, { USR_MSG_DISPLAY, EMAILS_COUNT } from './event-bus.service.js'
 import LoremIpsum from './loremIpsum.js'
 import emailFilter from '../cmps/email/email-filter.js'
 
@@ -37,7 +37,7 @@ function createEmail(i) {
     var email = {
         id: i,
         subject: loremIpsum.generate(utilService.getRandomInt(1, 4), utilService.getRandomInt(3, 6)),
-        sentAt: utilService.randomDate(new Date(2012, 0, 1), new Date()).valueOf(),  
+        sentAt: utilService.randomDate(new Date(2012, 0, 1), new Date()).valueOf(),
         body: loremIpsum.generate(utilService.getRandomInt(10, 30), utilService.getRandomInt(1, 4)),
         isRead: false,
     }
@@ -68,34 +68,43 @@ function sortByDate(emailA, emailB) {
 
 function deleteEmail(emailId) {
     var emails = storageService.load(EMAILS_KEY)
-    var emailIdx = emails.findIndex(email => email.id === emailId);    
+    var emailIdx = emails.findIndex(email => email.id === emailId);
     emails.splice(emailIdx, 1);
     storageService.store(EMAILS_KEY, emails);
-    return Promise.resolve({succes: true})
+    return Promise.resolve({ succes: true })
 }
 
 
-function emitEmailsCount(){
+function emitEmailsCount() {
     eventBus.$emit(EMAILS_COUNT, emailsDB.length);
 }
 
 
-function filterEmails(emails, filter = null){
+function filterEmails(emails, filter = null) {
     return new Promise((resolve, reject) => {
         var filteredEmails = emails.slice();
-
         console.log(filter);
         if (filter) {
-            var fromDate = filter.fromDate? new Date(filter.fromDate) : -Infinity;
-            var toDate = filter.toDate? new Date(filter.toDate) : Infinity;
-            filteredEmails = emails.filter(email => {
-            return email.subject.indexOf(filter.bySubject) !== -1
+            var fromDate = filter.fromDate ? new Date(filter.fromDate) : -Infinity;
+            var toDate = filter.toDate ? new Date(filter.toDate) : Infinity;
+            if (filter && filter.filterBy === 'read') filter.filterBy = true;
+            if (filter && filter.filterBy === 'unRead') filter.filterBy = false;
+
+            if (filter && filter.filterBy !== 'all') filteredEmails = emails.filter(email => {
+                return email.subject.indexOf(filter.bySubject) !== -1
                     && email.sentAt >= fromDate && email.sentAt <= toDate
                     // && email.isRead === filter.isRead
-            ;
-        });
-        }
-        resolve(filteredEmails);
+
+                // && email.isRead === filter.filterBy)
+            })
+            else if (filter && filter.filterBy === 'all') filteredEmails = emails.filter(email => {
+                return email.subject.indexOf(filter.bySubject) !== -1
+                    && email.sentAt >= fromDate && email.sentAt <= toDate
+                    // && email.isRead === filter.isRead
+            })
+    };
+
+resolve(filteredEmails);
     });
 }
 
@@ -104,7 +113,7 @@ function sendEmail() {
 }
 
 
-export default{
+export default {
     getEmails,
     createEmail,
     sortByTitle,
