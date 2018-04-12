f9a3222f7b7bc110cb1a4d237fe0a051f782c1ac
Ilana Mostovski <ilana.mostovski@gmail.com>
2018-04-12 15:05:44 +0300

    utils

--- js/pages/place/place-app.js
+++ js/pages/place/place-app.js
@@ -2,5 +2,7 @@ export default {
 
     template: `<section>
         <h1>place-app</h1>
+    
+        
     </section>`
 };
--- js/services/util.service.js
+++ js/services/util.service.js
@@ -41,11 +41,23 @@ function randomDate(start, end) {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 }
 
+function getParameterByName(name) {
+    var url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
 export default {
     getRandomInt,
     getRandomString : makeid,
     getCurrency,
     randomDate,
     getNextId,
+    getParameterByName
 }
 
