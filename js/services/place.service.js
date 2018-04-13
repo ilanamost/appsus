45b31033769843c1aed8074b654a853b3f1087b2
Nora Marcelli <noramarcelli@gmail.com>
2018-04-13 10:40:16 +0300

    add new places & update tags

--- js/services/place.service.js
+++ js/services/place.service.js
@@ -25,12 +25,12 @@ function getPlaces() {
 
 function generatePlaces() {
     var places = [];
-    var places =
+    places =
         [{
             id: 1,
             name: 'Alaska',
             desc: 'Alaska is a U.S. state located in the northwest extremity of North America.',
-            tags: ['Cold', 'Extrim'],
+            tags: ['Extrim', 'Fun', 'Romantic'],
             lat: 64.2008413,
             lng: -149.4936733,
             imgUrl: `img/15.jpg`
@@ -38,29 +38,29 @@ function generatePlaces() {
 
         {
             id: 2,
-            name: 'Times Squre',
+            name: 'Brooklyn Bridge',
             desc: 'Times Square is a major commercial intersection, tourist destination, entertainment center and neighborhood in the Midtown Manhattan section of New York City at the junction of Broadway and Seventh Avenue.',
-            tags: ['Fun', 'Family'],
+            tags: ['Romantic', 'Fun', 'Family', 'Children'],
             lat: 40.759011,
             lng: -73.9844722,
-            imgUrl: `img/2.jpg`
+            imgUrl: `img/8.jpg`
         },
 
         {
             id: 3,
             name: 'Ko Phi Phi',
             desc: 'The Phi Phi Islands are an island group in Thailand, between the large island of Phuket and the west Strait of Malacca coast of the mainland.',
-            tags: ['Relax', 'Family'],
+            tags: ['Fun', 'Relax', 'Romantic', 'Food'],
             lat: 7.740738,
             lng: 98.77841,
-            imgUrl: `img/3.jpg`
+            imgUrl: `img/1.jpg`
         },
 
         {
             id: 4,
             name: 'London Eye',
             desc: 'The London Eye, known for sponsorship reasons as the Coca-Cola London Eye, is a giant Ferris wheel on the South Bank of the River Thames in London.',
-            tags: ['Relax', 'Family', 'Fun'],
+            tags: ['Romantic', 'Relax', 'Family', 'Fun', 'Children'],
             lat: 51.503324,
             lng: -0.119543,
             imgUrl: `img/10.jpg`
@@ -70,11 +70,58 @@ function generatePlaces() {
             id: 5,
             name: 'Niagra Falls',
             desc: 'Niagara Falls is the collective name for three waterfalls that straddle the international border between the Canadian province Ontario and the American state of New York. ',
-            tags: ['Relax', 'Romantic'],
+            tags: ['Fun','Relax', 'Romantic', 'Family'],
             lat: 43.0828162,
             lng: -79.07416289999999,
+            imgUrl: `img/18.jpg`
+        },
+        {
+            id: 6,
+            name: 'Mexico city',
+            desc: 'Mexico City or Mexico FD is the capital city of Mexico, a large and populous city located in the central part of the country. The number of the dwellers is estimated to be close to 9 million people. Undoubtedly a key economic, political and cultural center of the country, Mexico City is filled with attractions and points of interest worth to be seen.',
+            tags: ['Dance', 'Family', 'Fun', 'Romantic', 'Children'],
+            lat: 19.432608,
+            lng: -99.133209,
+            imgUrl: `img/11.jpg`
+        },
+        {
+            id: 7,
+            name: 'Blue Spirit Costa Rica',
+            desc: 'Blue Spirit is one of the foremost world-class yoga and meditation retreat centers in Costa Rica, providing an extraordinary setting to all who are dedicated to spiritual transformation, personal growth, and environmental sustainability.',
+            tags: ['Fun', 'Dance', 'Family', 'Romantic'],
+            lat: 9.934739,
+            lng: -84.087502,
+            imgUrl: `img/14.jpg`
+        },
+        {
+            id: 8,
+            name: 'Kalathos, Rhodes',
+            desc: 'Kalathos is a small resort village in the Rhodes Island, southeastern Greece, located in eastern part of the island, near the town of Pefki. Despite the fact that the village is not located right near the seashore (but less than 1 miles to it), there are a few very good hotels and resorts for those who do not mind traveling a few minutes to the beaches. Certainly, a few very good resorts can be found at the very seashore as well.',
+            tags: ['Food', 'Family', 'Romantic', 'Children'],
+            lat: 36.121723,
+            lng: 28.059464,
             imgUrl: `img/5.jpg`
-        }];
+        },
+        {
+            id: 9,
+            name: 'Eilat',
+            desc: 'Eilat is the southernmost city in the State of Israel and the only one on the shores of the Red Sea, and serves as a central port and tourist city. The city is located in the south of the Arava, and belongs to the southern district, and has 50,000 residents.',
+            tags: ['Fun', 'Family', 'Romantic', 'Children', 'Dance', 'Extrim'],
+            lat: 32.109333,
+            lng: 34.855499,
+            imgUrl: `img/6.jpg`
+        },
+        {
+            id: 10,
+            name: 'Budapest',
+            desc: 'Budapest is a beautiful city and the capital city of Hungary, one of the most known tourist destinations in Europe and one of the most beautiful cities there. It was formed as a union of two cities, Buda and Pest, located on the opposite shores of the Danube river. The city is primarily known for its amazing architecture, with truly unforgettable cathedrals, the building of the Parliament, numerous markets and shops, wide streets and street lights, etc.',
+            tags: ['Fun', 'Family', 'Romantic', 'Children', 'Dance'],
+            lat: 47.497913,
+            lng: 19.040236,
+            imgUrl: `img/4.jpg`
+        },
+
+    ];
     return places;
 }
 
